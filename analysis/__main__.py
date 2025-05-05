4d56aa4aea4305adf7b24a2ec3d06abf57777ff8
umilojkovic <uroshm@gmail.com>
2025-05-05 17:57:48 -0400

    remove analysis dir.

--- analysis/__main__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import uvicorn
-
-# Add the parent directory to Python path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-if __name__ == "__main__":
-    uvicorn.run("analysis.main:app", host="0.0.0.0", port=8000, reload=True)    
