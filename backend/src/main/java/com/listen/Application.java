e64ae34cbe82793db7e340ad4d82ddbdaf26444f
umilojkovic <uroshm@gmail.com>
2025-05-05 20:52:09 -0400

    update app to not use pointless component scans.

--- backend/src/main/java/com/listen/Application.java
+++ backend/src/main/java/com/listen/Application.java
@@ -2,14 +2,8 @@ package com.listen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.listen.repositories")
-@EntityScan("com.listen.entity")
-@ComponentScan("com.listen")
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
